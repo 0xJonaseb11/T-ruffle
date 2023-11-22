5589dea14e58acae8c8cdd44d13caa8b43356e8c
jonas-sebera <sebejaz99@gmail.com>
2023-11-22 12:19:52 +0200

    init mailbox

---
+++ QuickStart/contracts/MailBox.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract MailBox {
+    string public mail;
+    uint public mailCount;
+    bool siInitialMail;
+    address senderAddress;
+
+    
+}
