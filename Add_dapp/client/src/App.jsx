eea4bc2479e6c57722c9ce6a2ad19031bb7ab2ec
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-16 13:44:08 +0200

    update init resolve

--- Add_dapp/client/src/App.jsx
+++ Add_dapp/client/src/App.jsx
@@ -1,76 +1,82 @@
-import react, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Web3 from "web3";
-
 import AdditionContract from "./Addition.json";
 
 const App = () => {
-  
   const [web3, setWeb3] = useState(null);
   const [contract, setContract] = useState(null);
+  const [number1, setNumber1] = useState("");
+  const [number2, setNumber2] = useState("");
   const [result, setResult] = useState("");
-  const [newResult, setNewResult] = useState();
-  const [account, setAccount] = useState();
+  const [account, setAccount] = useState("");
 
   useEffect(() => {
     const initializeWeb3 = async () => {
-    if (window.ethereum) {
-      const web3Instance = new Web3(window.ethereum);
-      try {
-        await window.ethereum.enable();
-        setWeb3(web3Instance);
+      if (window.ethereum) {
+        const web3Instance = new Web3(window.ethereum);
+        try {
+          await window.ethereum.enable();
+          setWeb3(web3Instance);
 
-        const accounts = await web3Instance.eth.getAccounts();
-        setAccount(accounts[0]);
+          const accounts = await web3Instance.eth.getAccounts();
+          setAccount(accounts[0]);
 
-        const networkId = await web3Instance.eth.net.getId();
-        const chainId = await web3Instance.eth.getChainId();
+          const networkId = await web3Instance.eth.net.getId();
+          const deployedNetwork = AdditionContract.networks[networkId];
 
-        const deployedNetwork = AdditionContract.networks[networkId];
+          if (deployedNetwork) {
+            const contractInstance = new web3Instance.eth.Contract(
+              AdditionContract.abi,
+              deployedNetwork.address
+            );
+            setContract(contractInstance);
 
-        const contractInstance = new web3Instance.eth.Contract(
-          AdditionContract.abi, 
-          deployedNetwork && deployedNetwork.address
-        );
-        setContract(contractInstance);
-
-        // Addition logic
-        const result = await contractInstance.methods.retrieveResult().call();
-        setResult(result);
-      } catch (error) {
-        console.log("Error connecting to MetaMask or getting account information");
+            const result = await contractInstance.methods.retrieveResult().call();
+            setResult(result);
+          } else {
+            console.log("Smart contract not deployed to the detected network.");
+          }
+        } catch (error) {
+          console.log("Error connecting to MetaMask or getting account information", error);
+        }
+      } else {
+        console.log("Ethereum browser not detected! Consider installing MetaMask instead!!");
       }
-    } else {
-      console.log("Ethereum browser not detected! Consider installing MetaMask instead!!");
-    }
-  };
-  
-  initializeWeb3();
- }, []);
+    };
 
- const handleAddition = async() => {
-  if (!contract) return;
+    initializeWeb3();
+  }, []);
+
+  const handleAddition = async () => {
+    if (!contract || !number1 || !number2) return;
     try {
-      await contract.methods.setResult(result).send({from: account});
+      await contract.methods.add(parseInt(number1), parseInt(number2)).send({ from: account });
       const updatedResult = await contract.methods.retrieveResult().call();
       setResult(updatedResult);
     } catch (error) {
-      console.log("Error getting result of provided numbers!!", error);
+      console.log("Error adding numbers:", error);
     }
- };
+  };
 
+  const handleNumber1Change = (event) => {
+    setNumber1(event.target.value);
+  };
 
-  const handleInputChange = ((event) => {
-   setResult(event.target.value);
-  });
+  const handleNumber2Change = (event) => {
+    setNumber2(event.target.value);
+  };
 
   return (
     <div className="App">
-      <h1 style>Want to add some positive numbers??</h1>
+      <h1>Want to add some positive numbers??</h1>
       <h1>Please Enter Two positive numbers:</h1>
-      <input type="number" value={result} onChange={handleInputChange}/>
+      <input type="number" value={number1} onChange={handleNumber1Change} />
+      <br />
+      <input type="number" value={number2} onChange={handleNumber2Change} />
       <button onClick={handleAddition}>Add</button>
+      <div>Result: {result}</div>
     </div>
   );
-}
+};
 
 export default App;
