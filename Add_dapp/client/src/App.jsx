0066e1fcfe5f5d7eda0dae55749c53fbb7d234f6
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-22 21:50:18 +0200

    init web3 --loadData

--- Add_dapp/client/src/App.jsx
+++ Add_dapp/client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import AdditionContract from './Addition.json';
 
@@ -10,34 +10,45 @@ const App = () => {
   const [num2, setNum2] = useState('');
   const [sum, setSum] = useState(null);
 
-  const loadWeb3 = async () => {
-    if (window.ethereum) {
-      const web3 = new Web3(window.ethereum);
+  useEffect(() => {
+    const initWeb3 = async () => {
+      let web3;
+      if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+      } else if (window.web3) {
+        web3 = new Web3(window.web3.currentProvider);
+      } else {
+        alert('Non-Ethereum browser detected. Consider installing MetaMask!');
+        return;
+      }
       setWeb3(web3);
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-    } else if (window.web3) {
-      const web3 = new Web3(window.web3.currentProvider);
-      setWeb3(web3);
-    } else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
-    }
-  };
+    };
 
-  const loadBlockchainData = async () => {
-    const web3 = window.web3;
-    const accounts = await web3.eth.getAccounts();
-    setAccount(accounts[0]);
-    const networkId = await web3.eth.net.getId();
-    const networkData = AdditionContract.networks[networkId];
-    if (networkData) {
-      const abi = AdditionContract.abi;
-      const address = networkData.address;
-      const contract = new web3.eth.Contract(abi, address);
-      setContract(contract);
-    } else {
-      window.alert('Smart contract not deployed to detected network.');
-    }
-  };
+    const loadBlockchainData = async (web3) => {
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+      const networkId = await web3.eth.net.getId();
+      const networkData = AdditionContract.networks[networkId];
+      if (networkData) {
+        const abi = AdditionContract.abi;
+        const address = networkData.address;
+        const contract = new web3.eth.Contract(abi, address);
+        setContract(contract);
+      } else {
+        alert('Smart contract not deployed to detected network.');
+      }
+    };
+
+    const init = async () => {
+      await initWeb3();
+      if (web3) {
+        await loadBlockchainData(web3);
+      }
+    };
+
+    init();
+  }, [web3]);
 
   const handleAddition = async (e) => {
     e.preventDefault();
@@ -47,14 +58,10 @@ const App = () => {
     }
   };
 
-  React.useEffect(() => {
-    loadWeb3();
-    loadBlockchainData();
-  }, []);
-
   return (
     <div>
       <h1>Web3 Addition DApp</h1>
+      <p>Account: {account}</p>
       <form onSubmit={handleAddition}>
         <div>
           <label>Number 1: </label>
