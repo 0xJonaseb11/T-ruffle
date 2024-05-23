51eef73bb891745895d29956ce37b5c2d0089bee
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-23 10:16:11 +0200

    update init --resolved

--- Add_dapp/contracts/Addition.sol
+++ Add_dapp/contracts/Addition.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 contract Addition {
     
     // Function to add two numbers and return the sum
-    function add(uint256 num1, uint256 num2) public pure returns (uint256) {
+    function add(uint256 num1, uint256 num2) public view returns (uint256) {
         return num1 + num2;
     }
 }
